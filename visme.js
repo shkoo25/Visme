c8813ce31866bf5efe67079e37a98091e827cd71
Steve Koo <shkoo25@gmail.com>
2015-08-26 12:55:39 -0400

    Was able to load JSON data, but did was not able to filter or create a search functionality due to time.

---
+++ visme.js
@@ -0,0 +1,84 @@
+var State = Backbone.Model.extend({
+
+ 
+})
+
+var Collection = Backbone.Collection.extend({
+
+  model: State
+
+})
+
+var template = Handlebars.compile( $("#state-template").html() )
+
+var updateViews = function(collection) {
+  $("#state-container").empty()
+
+  collection.each(function(student){
+    $("#state-container").append( template(student.toJSON()) )
+  })
+}
+
+
+
+$(document).on("ready", function(){
+  states = new Collection([ 
+    { state: "Alabama" }, 
+    { state: "Alaska" },
+    { state: "Arizona" },
+    { state: "Arkansas" },
+    { state: "California" },
+    { state: "Colorado" },
+    { state: "Connecticut" },
+    { state: "Delaware" },
+    { state: "Florida" },
+    { state: "Georgia" },
+    { state: "Hawaii" },
+    { state: "Idaho" },
+    { state: "Illinois" },
+    { state: "Indiana" },
+    { state: "Iowa" },
+    { state: "Kansas" },
+    { state: "Kentucky" },
+    { state: "Louisiana" },
+    { state: "Maine" },
+    { state: "Maryland" },
+    { state: "Massachusetts" },
+    { state: "Michigan" },
+    { state: "Minnesota" },
+    { state: "Mississippi" },
+    { state: "Missouri" },
+    { state: "Montana" },
+    { state: "Nebraska" },
+    { state: "Nevada" },
+    { state: "New Hampshire" },
+    { state: "New Jersey" },
+    { state: "New Mexico" },
+    { state: "New York" },
+    { state: "North Carolina" },
+    { state: "North Dakota" },
+    { state: "Ohio" },
+    { state: "Oklahoma" },
+    { state: "Oregon" },
+    { state: "Pennsylvania" },
+    { state: "Rhode Island" },
+    { state: "South Carolina" },
+    { state: "South Dakota" },
+    { state: "Tennessee" },
+    { state: "Texas" },
+    { state: "Utah" },
+    { state: "Vermont" },
+    { state: "Virginia" },
+    { state: "Washington" },
+    { state: "West Virginia" },
+    { state: "Wisconsin" },
+    { state: "Wyoming" }
+   
+  ])
+
+  updateViews(states)
+
+
+})
+
+	
